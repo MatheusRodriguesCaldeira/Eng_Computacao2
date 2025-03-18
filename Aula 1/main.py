56f4bd23391247a0622247986bb16b790efea38e
Matheus Rodrigues Caldeira <133373577+MatheusRodriguesCaldeira@users.noreply.github.com>
2025-03-17 22:47:13 -0300

    Add files via upload

---
+++ Aula 1/main.py
@@ -0,0 +1,14 @@
+print ("Matheus")
+
+a = 3
+b = 2
+print (2*a*3*b)
+
+# nun1 = 2
+# nun2 = 3
+# nun3 = 4
+# soma = num1+num2+num3
+# print(soma)
+
+nun1 = input ("numero 1 = ")
+nun1 = input ("numero 1 = ")
---
+++ Aula 1/projeto_ofc.py
@@ -0,0 +1,17 @@
+print ("Matheus")
+
+a = 3
+b = 2
+print (2*a*3*b)
+
+# nun1 = 2
+# nun2 = 3
+# nun3 = 4
+# soma = num1+num2+num3
+# print(soma)
+
+nun1 = float(input ("numero 1 = "))
+nun1 = float(input ("numero 2 = "))
+nun1 = float(input ("numero 3 = "))
+soma = nun1+nun2+nun3
+print("soma= {soma:2f}")
